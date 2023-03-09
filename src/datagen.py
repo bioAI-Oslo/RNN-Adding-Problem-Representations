3b7176c8bc2d9908cbc3cbb3efdc118f6c244ded
Mattis Dalsætra <mattisdo@uio.no>
2023-03-09 17:05:24 +0100

    Progress on RNN linear counting

--- src/datagen.py
+++ src/datagen.py
@@ -22,9 +22,25 @@ def datagen(n_data,t_steps,n_masks):
     labels = torch.sum(data[:, :, 0]*data[:, :, 1], dim=1)
     return data,labels
 
+
 def datagen_full_sum(n_data,t_steps,normalize=True):
+    data = torch.zeros((n_data, t_steps))
+    for i in range(n_data):
+        # Rayleigh distribution for the random numbers to sum, scale=1
+        data[i] = torch.tensor(np.random.rayleigh(0.1, t_steps))
+        if normalize:
+            data[i] = data[i]/sum(data[i])
+    # To fit into model
+    data = data.unsqueeze(2)
+    labels = torch.zeros(n_data)
+    labels = torch.sum(data, dim=1)
+    labels = labels.squeeze()
+    return data,labels
+
+def datagen_full_sum_normal(n_data,t_steps,normalize=True):
     data = torch.rand((n_data, t_steps)).round(decimals=2)
     for i in range(n_data):
+        # Normal distribution for random numbers to sum
         data[i] = torch.empty(t_steps).normal_(0.1,0.05)
         data[i] = abs(data[i])
         if normalize:
@@ -32,6 +48,7 @@ def datagen_full_sum(n_data,t_steps,normalize=True):
     data = data.unsqueeze(2)
     labels = torch.zeros(n_data)
     labels = torch.sum(data, dim=1)
+    labels = labels.squeeze()
     return data,labels
 
 def datagen_timewise_labels(n_data,t_steps,n_masks):
