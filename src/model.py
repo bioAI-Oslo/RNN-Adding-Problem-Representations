1fe793634d835f34cb2d4ffb5c42ef2ce86796e8
Mattis Dalsætra <mattisdo@uio.no>
2023-04-27 17:10:50 +0200

    End progress 27.04.23

--- src/model.py
+++ src/model.py
@@ -636,3 +636,134 @@ class RNN_circular_ND_pm(torch_RNN_full_manual):
         plt.legend()
         plt.title("MSE Losses")
         plt.show()
+
+
+
+class RNN_circular_LowEtAl(nn.Module):
+    def __init__(self,input_size,hidden_size,lr=0.001,irnn=False,outputnn=False,bias=False,Wx_normalize=False,activation=False,batch_size=64,nav_space=1):
+        super().__init__()
+        self.input_size = input_size
+        # self.time_steps = time_steps
+        self.hidden_size = hidden_size
+        self.nav_space = nav_space # Number of navigation dimensions, default 1D, if 2D we want torus
+
+        self.hidden = torch.nn.Linear(hidden_size,hidden_size,bias=False)
+        self.input = torch.nn.Linear(input_size,hidden_size,bias=bias)
+
+        self.h0_layer = torch.nn.Linear(self.nav_space,hidden_size,bias=bias)
+
+        self.outputnn = outputnn
+        self.output = torch.nn.Linear(hidden_size,2*self.nav_space,bias=bias)
+
+        self.batch_size = batch_size
+
+        self.base_training_tsteps = 20
+
+        # Initialize IRNN
+        self.activation = activation
+        if self.activation:
+            self.act = torch.nn.ReLU()
+        else:
+            self.act = torch.nn.Identity()
+
+        if irnn:
+            torch.nn.init.eye_(self.hidden.weight)
+            if bias:
+                # torch.nn.init.constant_(self.hidden.bias, 0)
+                torch.nn.init.constant_(self.input.bias, 0)
+
+            
+        if Wx_normalize:
+        # Set norm of weights to 1
+            self.input.weight = torch.nn.Parameter(self.input.weight/torch.norm(self.input.weight,dim=0).unsqueeze(1),requires_grad=True)
+            # Freeze weights
+            self.input.weight.requires_grad = False
+            # print('Wx_norm: ',torch.norm(self.input.weight,dim=0).unsqueeze(1))
+
+        self.Wh_init = self.hidden.weight.detach().clone()
+        self.Wx_init = self.input.weight.detach().clone()
+
+        self.loss_func = torch.nn.MSELoss()
+
+        self.lr = lr
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=0.01)
+
+        self.losses = []
+        self.accs = []
+
+    def forward(self, x, raw=False):
+        # Make h0 trainable
+        batch_size_forward = x.size(0)
+        theta0s = torch.rand((batch_size_forward,self.nav_space))*2*np.pi
+        h = self.h0_layer(theta0s) # Usikker på denne
+        time_steps = x.size(1)
+        # time_steps+1 because we want to include the initial hidden state
+        self.hts = torch.zeros(time_steps+1, batch_size_forward, self.hidden_size)
+        self.hts[0] = h
+        # Main RNN loop
+        for t in range(0,time_steps):
+            h = self.act(self.hidden(h) + self.input(x[:,t,:]))
+            self.hts[t+1] = h
+        # If outputnn is true, use a linear layer to output the hs
+        if not raw:
+            return self.output(self.hts)
+        # Else, output all hts
+        else:
+            return self.hts
+
+    def loss_fn(self, x, y_hat):
+        y = self(x)[1:,:,:]
+        y_hat = y_hat.transpose(0,1)
+        loss_sin = self.loss_func(y[:,:,0],y_hat[:,:,0])
+        loss_cos = self.loss_func(y[:,:,1],y_hat[:,:,1])
+        loss = loss_sin + loss_cos
+        self.losses.append(loss.item())
+        return loss
+
+    def train_step(self, x, y_hat):
+        self.optimizer.zero_grad(set_to_none=True)
+        # Backward hook to clip gradients
+        loss = self.loss_fn(x, y_hat)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.parameters(), 2.0) # Clip gradients as in paper Low et al.
+        self.optimizer.step()
+        # Print weight gradient norms
+        # print("Hidden weight grad norm:",torch.norm(self.hidden.weight.grad))
+        return loss.item()
+
+    def train(self, epochs=100, loader=None):
+        for epoch in tqdm(range(epochs)):
+            data,labels = datagen_lowetal(self.batch_size,self.base_training_tsteps)
+            loss = self.train_step(data,labels)
+        return self.losses
+    
+    def train_gradual(self, epochs=100, loader=None):
+        i = 0
+        training_steps = 1
+        for epoch in tqdm(range(epochs)):
+            if i%50 == 0:
+                training_steps += 1
+            data,labels = datagen_lowetal(self.batch_size,training_steps)
+            loss = self.train_step(data,labels)
+            i+=1
+        return self.losses
+    
+    def plot_losses(self,average=None):
+        losses = np.array(self.losses)
+        if average == None:
+            plt.plot(losses[10:])
+        else:
+            if len(losses)%average != 0:
+                losses = losses[:-(len(losses)%average)]
+                print("Losses array was not a multiple of average. Truncated to",len(losses))
+            loss_data_avgd = losses.reshape(average,-1).mean(axis=1)
+            plt.plot(loss_data_avgd[3:])
+        plt.title("MSE Losses")
+        plt.show()
+
+    def plot_accs(self):
+        accs = np.array(self.accs)
+        acc_data_avgd = accs.reshape(10,-1).mean(axis=1)
+        plt.plot(accs)
+        plt.title("Accuracy on training data")
+        plt.show()
