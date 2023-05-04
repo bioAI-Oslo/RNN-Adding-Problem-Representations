a36169630748464fb48547b8ba303b5b781eae88
Mattis Dalsætra <mattisdo@uio.no>
2023-05-04 17:06:57 +0200

    Low et al bridged to mine

--- src/model.py
+++ src/model.py
@@ -768,28 +768,105 @@ class RNN_circular_LowEtAl(nn.Module):
         plt.title("Accuracy on training data")
         plt.show()
 
-class RNN_circular_LowEtAl_reduced(RNN_circular_LowEtAl):
+class RNN_circular_LowEtAl_bridged(RNN_circular_LowEtAl):
     # Goal of reducing the model complexity of the parent class
-    def __init__(self,input_size,hidden_size,lr=0.001,irnn=True,outputnn=False,bias=False,Wx_normalize=False,activation=True,batch_size=64,nav_space=1):
+    def __init__(self,input_size,hidden_size,lr=0.001,act_decay=0.01,irnn=True,outputnn=False,bias=False,Wx_normalize=False,activation=True,batch_size=64,nav_space=1):
         super().__init__(input_size,hidden_size,lr=0.001,irnn=irnn,outputnn=outputnn,bias=bias,Wx_normalize=Wx_normalize,activation=activation,batch_size=batch_size,nav_space=nav_space)
-        self.output = torch.nn.Linear(hidden_size,2*self.nav_space,bias=True)
+        self.act_decay = act_decay
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=0)
+
+        self.losses_norm = []
 
     def forward(self, x, raw=False):
         # Make h0 trainable
         batch_size_forward = x.size(0)
-        theta0s = torch.ones((batch_size_forward,self.nav_space)) # Gives constant initial hidden state
-        h = self.h0_layer(theta0s)
-        time_steps = x.size(1)
+        h = torch.zeros((batch_size_forward,self.hidden_size)) # Gives constant initial hidden state
+        h[:,0] = 1
+        self.time_steps = x.size(1)
         # time_steps+1 because we want to include the initial hidden state
-        self.hts = torch.zeros(time_steps+1, batch_size_forward, self.hidden_size)
+        self.hts = torch.zeros(self.time_steps+1, batch_size_forward, self.hidden_size)
         self.hts[0] = h
         # Main RNN loop
-        for t in range(0,time_steps):
+        for t in range(0,self.time_steps):
             h = self.act(self.hidden(h) + self.input(x[:,t,:]))
             self.hts[t+1] = h
-        # If outputnn is true, use a linear layer to output the hs
-        if not raw:
-            return self.output(self.hts)
-        # Else, output all hts
+
+        return self.hts
+    
+    def loss_fn(self, x, y_hat):
+        y = self(x)
+        # norm of hts at each time step regularized to be 1
+        activity_L2 = self.act_decay*((torch.norm(y,dim=-1)-1)**2).sum()
+        
+        # Concatenate 0 (h0) to y_hat to make it the same size as y
+        y_hat = torch.cat((torch.zeros(y_hat.size(0),1),y_hat),dim=1)
+        # Permute y_hat to make it the same size as y
+        y_hat = y_hat.permute(1,0)
+        # angle_loss = 0
+        
+        # Main angle loss loop, checks difference in angles for multiple time steps back in time
+        i = torch.arange(1, self.time_steps).unsqueeze(1)
+        j = torch.arange(1, self.time_steps//2-int(self.time_steps*0.1)).unsqueeze(0)
+        # THIS IS VERY UNCERTAIN, TRY ALSO >= (BUT I THINK IT SHOULD BE >)
+        mask = (i > j).float()
+        j = j * mask
+        # Convert i and j to int
+        i = i.long()
+        j = j.long()
+        normalizer = 1 / (torch.norm(y[i], dim=-1) * torch.norm(y[i-j], dim=-1))
+        # Cant clamp between -1 and 1 because it will cause NaNs in training
+        angle_test = torch.abs(torch.acos(torch.clamp(torch.sum(y[i]*y[i-j], dim=-1) * normalizer, -0.9999999, 0.9999999)))
+        # Must use torch.abs because the angle can be negative, but the angle_test only returns positive angles
+        angle_theoretical = torch.abs(y_hat[i]-y_hat[i-j])
+        # Make 0 and 2pi the same angle
+        # angle_theoretical = torch.min(torch.remainder(2*np.pi-(y_hat[i]-y_hat[i-j]),2*np.pi),torch.remainder(2*np.pi-(y_hat[i-j]-y_hat[i]),2*np.pi))
+        angle_loss = torch.mean((angle_test-angle_theoretical)**2)
+
+        # Loss to end in the same position as the start
+        # circle_end_loss = 0.0001*torch.mean((y[-1]-y[0])**2)
+
+        self.losses.append(angle_loss.item())
+        self.losses_norm.append(activity_L2.item())
+        # self.losses_circle.append(circle_end_loss.item())
+        loss = angle_loss + activity_L2 # + circle_end_loss
+        return loss
+    
+    def plot_losses(self,average=None):
+        losses = np.array(self.losses)
+        losses_norm = np.array(self.losses_norm)
+        if average == None:
+            plt.plot(losses[10:],label="Angle Loss")
+            plt.plot(losses_norm[10:], label="Activity norm Loss")
+            plt.plot(losses[10:]+losses_norm[10:], label="Total Loss")
         else:
-            return self.hts
+            if len(losses)%average != 0:
+                losses = losses[:-(len(losses)%average)]
+                losses_norm = losses_norm[:-(len(losses_norm)%average)]
+                print("Losses array was not a multiple of average. Truncated to",len(losses))
+            loss_data_avgd = losses.reshape(average,-1).mean(axis=1)
+            loss_data_norm_avgd = losses_norm.reshape(average,-1).mean(axis=1)
+            plt.plot(loss_data_avgd[3:], label="Angle Loss")
+            plt.plot(loss_data_norm_avgd[3:], label="Activity norm Loss")
+            plt.plot(loss_data_avgd[3:]+loss_data_norm_avgd[3:], label="Total Loss")
+        plt.legend()
+        plt.title("MSE Losses")
+        plt.show()
+
+
+    def train(self, epochs=100, loader=None):
+        for epoch in tqdm(range(epochs)):
+            data,labels, _ = datagen_lowetal_direct(self.batch_size,self.base_training_tsteps)
+            loss = self.train_step(data,labels)
+        return self.losses
+    
+    def train_gradual(self, epochs=100, loader=None):
+        i = 0
+        training_steps = 1
+        for epoch in tqdm(range(epochs)):
+            if i%50 == 0:
+                training_steps += 1
+            data,labels,_ = datagen_lowetal_direct(self.batch_size,training_steps)
+            loss = self.train_step(data,labels)
+            i+=1
+        print("Last training time steps:",training_steps)
+        return self.losses
